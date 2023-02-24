acf09ce3c526fdbfce8c39bc5bdda62dfca145c4
S V Jagadeesh <vjagadeesh083@gmail.com>
2023-02-24 12:44:41 +0530

    initial commit for github actions  gke

---
+++ src/main/java/com/aeradroncode/githubactionsgke1/GithubActionsGke1Application.java
@@ -0,0 +1,13 @@
+package com.aeradroncode.githubactionsgke1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsGke1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsGke1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/aeradroncode/githubactionsgke1/GithubActionsGke1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeradroncode.githubactionsgke1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsGke1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
